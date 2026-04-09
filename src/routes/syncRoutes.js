4e17a728df318145feb76db1e5e023a09ff0434a
lokeshramchand-ctrl <lokeshramchand@gmail.com>
2026-04-09 04:11:35 +0000

    refactor: remove syncRoutes and update nlpService to use localhost for predictions

--- src/routes/syncRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { syncGmail } = require('../controllers/syncController');
-
-const router = express.Router();
-
-router.post('/', syncGmail);
-
-module.exports = router;
--- src/services/nlpService.js
+++ src/services/nlpService.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 exports.predictCategory = async (description) => {
   try {
    //cd const res = await axios.post('http://192.168.1.100:5000/api/predict', { description });
-    const res = await axios.post('http://nlpservice:5000/api/predict', { description });
+    const res = await axios.post('http://localhost:5000/api/predict', { description });
     return res.data.category || 'Other';
   } catch (err) {
     console.error('NLP Service Error:', err.message);
